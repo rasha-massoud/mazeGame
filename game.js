65d62dd10ec3a92f4fae5d3b4b0a4bb9c13246c5
RashaM <rashamassoud1998@gmail.com>
2023-03-08 23:51:24 +0200

    Connect the HTML file to the JavaScript when the link is on the header

---
+++ game.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+
+    document.getElementById("start").addEventListener("mouseover", () => {
+        const start = true;
+    });
+
+    if (start) {
+
+        
+        document.getElementsByClassName("boundary").addEventListener("mouseover", ()=>{
+
+        });
+    }
+
+
+}
