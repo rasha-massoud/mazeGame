8b256fc66069375023edf46968fb0e20e321db3b
RashaM <rashamassoud1998@gmail.com>
2023-03-09 01:30:27 +0200

    Add the reset feature

--- game.js
+++ game.js
@@ -1,17 +1,29 @@
 window.onload = function () {
-
+    var start = false;
     document.getElementById("start").addEventListener("mouseover", () => {
-        const start = true;
         document.querySelectorAll(".boundary").forEach((boundary) => {
             boundary.addEventListener("mouseover", () => {
                 document.querySelectorAll(".boundary").forEach((boundary) => {
-                    boundary.classList.add("youlose");
-                    document.getElementById("status").innerText="Ouppss! You Lost.";
+                    boundary.style.backgroundColor = "#ff8888";
+
+                    document.getElementById("status").innerText = "Ouppss! You Lost.";
+                    start = false;
+                    console.log(start);
                 });
             });
-        });
 
-    })
+        });
+    });
+    if (start == false) {
+        console.log(start, "HIIIIII");
+        document.getElementById("start").addEventListener("click", () => {
+            document.querySelectorAll(".boundary").forEach((boundary) => {
+                boundary.style.backgroundColor = "#eeeeee";
+                start = true;
+                document.getElementById("status").innerText = "Begin by moving your mouse over the \"S\" ";
+            });
+        });
+    }
 
 
 
