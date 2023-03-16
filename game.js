f145b898f15ddf62b592e1349fd4d07b6cde39d8
RashaM <rashamassoud1998@gmail.com>
2023-03-16 19:28:35 +0200

    Updated the winning score, text and condition

--- game.js
+++ game.js
@@ -11,11 +11,26 @@ window.onload = function () {
                     boundary.classList.add("youlose");
                     once += 6;
                     if (once == 6) {
-                        start = updateTextandScore("Ouppss! You Lost.", score - 10, false);
+                        start = false;
+                        updateTextandScore("Ouppss! You Lost.", score - 10);
                     }
                 });
             });
         });
+    });
+
+
+    let div = document.getElementById('start');
+    let isDragging = false;
+    let offset = { x: 0, y: 0 };
+
+    // Add mousedown event listener to the div
+    div.addEventListener('mousedown', function (e) {
+        isDragging = true;
+
+        // Calculate the offset between the mouse position and the div position
+        offset.x = e.clientX - div.offsetLeft;
+        offset.y = e.clientY - div.offsetTop;
     });
 
     if (!start) {
@@ -24,60 +39,43 @@ window.onload = function () {
                 boundary.classList.remove("youlose");
             });
             once = 0;
-            start = updateTextandScore("Begin by moving your mouse over the \"S\" ", 0, true);
+            start = true;
+            updateTextandScore("Begin by moving your mouse over the \"S\" ", 0);
         });
     }
 
-    // //UPDATE NOT TO GET OUT FROM THE MAZEEE
-    // let div = document.getElementById('start');
-    // let isDragging = false;
-    // let offset = { x: 0, y: 0 };
+    // Add mousemove event listener to the document
+    document.addEventListener('mousemove', function (e) {
+        if (isDragging) {
+            // Update the position of the div to follow the mouse
+            Xaxis = (e.clientX - offset.x) + 'px';
+            Yaxis = (e.clientY - offset.y) + 'px';
+            div.style.left = (e.clientX - offset.x) + 'px';
+            div.style.top = (e.clientY - offset.y) + 'px';
+        }
+    });
 
-    // // Add mousedown event listener to the div
-    // div.addEventListener('mousedown', function (e) {
-    //     isDragging = true;
+    // Add mouseup event listener to the document
+    document.addEventListener('mouseup', function (e) {
+        isDragging = false;
+    });
 
-    //     // Calculate the offset between the mouse position and the div position
-    //     offset.x = e.clientX - div.offsetLeft;
-    //     offset.y = e.clientY - div.offsetTop;
-    // });
+    document.getElementById("start").addEventListener("click", () => {
+        const endRect = document.getElementById('end').getBoundingClientRect();
+        const startRect = document.getElementById('start').getBoundingClientRect();
 
-    // // Add mousemove event listener to the document
-    // document.addEventListener('mousemove', function (e) {
-    //     if (isDragging) {
-    //         // Update the position of the div to follow the mouse
-    //         div.style.left = (e.clientX - offset.x) + 'px';
-    //         div.style.top = (e.clientY - offset.y) + 'px';
-    //     }
-    // });
+        if (startRect.right >= endRect.left &&
+            startRect.left < endRect.right &&
+            startRect.bottom >= endRect.top &&
+            startRect.top <= endRect.bottom) {
+            updateTextandScore("You Won.", score + 5);
 
-    // // Add mouseup event listener to the document
-    // document.addEventListener('mouseup', function (e) {
-    //     isDragging = false;
-    // });
+        }
 
-    // const endRect = document.getElementById('end').getBoundingClientRect();
-    // const startRect = document.getElementById('start').getBoundingClientRect();
+    });
 
-    // document.getElementsByTagName("body")[0].addEventListener("click", () => {
-    //     let endRect = document.getElementById('end').getBoundingClientRect();
-    //     let startRect = document.getElementById('start').getBoundingClientRect();
-
-    //     const isOverlap = !(
-    //         endRect.left > startRect.right &&
-    //         endRect.left < startRect.right + 0.5
-    //     );
-    //     // console.log(endRect.left, startRect.right, endRect.right, startRect.left, endRect.top, startRect.bottom, endRect.bottom, startRect.top);
-    //     if (isOverlap) {  //ANOTHER FLAG like start
-    //         document.getElementById("status").innerText = "You Won. "; //STOP GAME THEN SCORE
-    //         // score += 5;
-    //         document.getElementsByClassName("example")[0].innerText = score;
-    //     }
-    // });
-
-    const updateTextandScore = (text, score, start) => {
+    const updateTextandScore = (text, score) => {
         document.getElementById("status").innerText = text;
         document.getElementsByClassName("example")[0].innerText = score;
-        return start;
     }
 }
